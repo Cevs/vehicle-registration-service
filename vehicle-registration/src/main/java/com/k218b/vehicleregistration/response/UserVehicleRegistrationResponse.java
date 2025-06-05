e9ae1255869a1d5022a0bcb8874f7eb2ea5c61b4
alenmartincevic <Alen.Martincevic@partner.bmw.de>
2025-06-05 09:23:32 +0200

    push leftover

--- vehicle-registration/src/main/java/com/k218b/vehicleregistration/response/UserVehicleRegistrationResponse.java
+++ vehicle-registration/src/main/java/com/k218b/vehicleregistration/response/UserVehicleRegistrationResponse.java
@@ -1,3 +1,20 @@
 package com.k218b.vehicleregistration.response;
 
-public record UserVehicleRegistrationResponse(String validUntil, String message) {}
+public class UserVehicleRegistrationResponse extends HttpResponse{
+	private final String validUntil;
+	private final String message;
+
+	public UserVehicleRegistrationResponse(String validUntil, String message, int statusCode) {
+		super(statusCode);
+		this.validUntil = validUntil;
+		this.message = message;
+	}
+
+	public String validUntil() {
+		return validUntil;
+	}
+
+	public String message() {
+		return message;
+	}
+}
